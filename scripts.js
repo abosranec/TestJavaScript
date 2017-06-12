782adb4f6e137a785a7b20bc7e80ccfd179bdb23
pasha <pasha_mugako@mail.ru>
2017-06-12 16:12:51 +0300

    Create repository

---
+++ scripts.js
@@ -0,0 +1,19 @@
+var myObj = {
+	obj1: 1,
+	obj2: "aaa",
+	obj3: true,
+	setObj1:function(obj1){
+		this.obj1 = obj1;
+	},
+	getObj1:function(){
+		return this.obj1;
+	}
+};
+
+for(var obj in myObj){
+	document.writeln(""+obj);
+}
+myObj.setObj1(4);
+document.writeln(myObj.getObj1());
+var logo = document.body.querySelector(".icon");
+logo.src = "knead.gif";
